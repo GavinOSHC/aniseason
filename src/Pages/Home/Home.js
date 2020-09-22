eb6cedcea9f29328f7dd83c980a95c770ba58cdc
gavin <gavinoshc@gmail.com>
2020-09-22 22:59:54 +0100

    mapped state to props

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Nav from "../../components/nav/Nav";
+import { connect } from "react-redux";
+import { fetchAnime } from "../../actions";
 
 import "./Home.scss";
 
-const Home = () => {
-	return (
-		<div className="home-page">
-			<Nav />
+const Home = ({ anime, loading, error, dispatch }) => {
+	const [year, setYear] = useState(2020);
+	const [season, setSeason] = useState("winter");
+
+	useEffect(() => {
+		dispatch(fetchAnime(season, year));
+	}, [year, season]);
+
+	const renderLoading = () => {
+		if (error) {
+			console.log("error");
+		}
+		return <div>Loading....</div>;
+	};
+
+	const renderError = () => {
+		return <div>Error</div>;
+	};
+
+	const renderGrid = () => {
+		if (error) return renderError();
+		console.log(anime);
+		return (
 			<section className="anime-chart">
 				<div className="anime-container">
 					<div className="TV">
@@ -20,8 +41,23 @@ const Home = () => {
 					</div>
 				</div>
 			</section>
+		);
+	};
+
+	return (
+		<div className="home-page">
+			<Nav />
+			{loading ? renderLoading() : renderGrid()}
 		</div>
 	);
 };
 
-export default Home;
+const mapstateToProps = (state) => {
+	return {
+		anime: state.animeReducer.anime,
+		loading: state.animeReducer.loading,
+		error: state.animeReducer.error,
+	};
+};
+
+export default connect(mapstateToProps)(Home);
