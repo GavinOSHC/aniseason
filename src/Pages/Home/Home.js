fe9c5002e0b30d6778e83a4740d0d2cb0c1c1fca
gavin dhaliwal <gavinoshc@gmail.com>
2020-09-23 20:30:00 +0100

    rending anime titles

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -6,58 +6,69 @@ import { fetchAnime } from "../../actions";
 import "./Home.scss";
 
 const Home = ({ anime, loading, error, dispatch }) => {
-	const [year, setYear] = useState(2020);
-	const [season, setSeason] = useState("winter");
+  const [year, setYear] = useState(2020);
+  const [season, setSeason] = useState("winter");
 
-	useEffect(() => {
-		dispatch(fetchAnime(season, year));
-	}, [year, season]);
+  useEffect(() => {
+    dispatch(fetchAnime(season, year));
+  }, [year, season]);
 
-	const renderLoading = () => {
-		if (error) {
-			console.log("error");
-		}
-		return <div>Loading....</div>;
-	};
+  const renderLoading = () => {
+    if (error) {
+      console.log("error");
+    }
+    return <div>Loading....</div>;
+  };
 
-	const renderError = () => {
-		return <div>Error</div>;
-	};
+  const renderError = () => {
+    return <div>Error</div>;
+  };
 
-	const renderGrid = () => {
-		if (error) return renderError();
-		console.log(anime);
-		return (
-			<section className="anime-chart">
-				<div className="anime-container">
-					<div className="TV">
-						<h3 className="white">TV</h3>
-					</div>
-					<div className="Movies">
-						<h3 className="white">TV</h3>
-					</div>
-					<div className="ONA">
-						<h3 className="white">TV</h3>
-					</div>
-				</div>
-			</section>
-		);
-	};
+  const renderGrid = () => {
+    if (error) return renderError();
+    return (
+      <section className="anime-chart">
+        <div className="anime-container">
+          <div className="TV">
+            <h3 className="white">TV</h3>
+            {renderTVanime()}
+          </div>
+          <div className="Movies">
+            <h3 className="white">TV</h3>
+          </div>
+          <div className="ONA">
+            <h3 className="white">TV</h3>
+          </div>
+        </div>
+      </section>
+    );
+  };
 
-	return (
-		<div className="home-page">
-			<Nav />
-			{loading ? renderLoading() : renderGrid()}
-		</div>
-	);
+  const renderTVanime = () => {
+    const tv = anime.anime.filter(
+      (anime) => anime.attributes.showType === "TV"
+    );
+
+    console.log(tv);
+    return tv.map(({ id, attributes }) => (
+      <p key={id}>{attributes.titles.en_jp}</p>
+    ));
+  };
+
+  return (
+    <div className="home-page">
+      <Nav />
+      {loading ? renderLoading() : renderGrid()}
+    </div>
+  );
 };
 
 const mapstateToProps = (state) => {
-	return {
-		anime: state.animeReducer.anime,
-		loading: state.animeReducer.loading,
-		error: state.animeReducer.error,
-	};
+  return {
+    anime: state.animeReducer.anime,
+    loading: state.animeReducer.loading,
+    error: state.animeReducer.error,
+  };
 };
 
 export default connect(mapstateToProps)(Home);
