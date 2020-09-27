ced8d74e9550b4da87c763b2875903e4e5addbb1
gavin dhaliwal <gavinoshc@gmail.com>
2020-09-27 17:58:30 +0100

    added mobile nav

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,158 +8,158 @@ import Skeleton from "@material-ui/lab/Skeleton";
 import "./Home.scss";
 
 const Home = ({ anime, loading, error, dispatch, year, season }) => {
-	useEffect(() => {
-		dispatch(fetchAnime(season, year));
-	}, [dispatch, year, season]);
+    useEffect(() => {
+        dispatch(fetchAnime(season, year));
+    }, [dispatch, year, season]);
 
-	const test = [1, 2, 3, 4, 5, 6];
+    const renderSkeleton = () => {
+        return Array.from(new Array(15)).map((item) => {
+            return (
+                <Skeleton className="skeleton" variant="rect">
+                    <AnimeCard />
+                </Skeleton>
+            );
+        });
+    };
 
-	const renderSkeleton = () => {
-		return Array.from(new Array(15)).map((item) => {
-			return (
-				<Skeleton className="skeleton" variant="rect">
-					<AnimeCard />
-				</Skeleton>
-			);
-		});
-	};
+    const renderLoading = () => {
+        if (error) {
+            return renderError();
+        }
+        return (
+            <Grid
+                container
+                justify="center"
+                direction="row"
+                alignItems="center"
+                spacing={2}
+            >
+                {renderSkeleton()}
+            </Grid>
+        );
+    };
 
-	const renderLoading = () => {
-		if (error) {
-			return renderError();
-		}
-		return (
-			<Grid
-				container
-				justify="center"
-				direction="row"
-				alignItems="center"
-				spacing={2}
-			>
-				{renderSkeleton()}
-			</Grid>
-		);
-	};
+    const renderError = () => {
+        return <div>Error</div>;
+    };
 
-	const renderError = () => {
-		return <div>Error</div>;
-	};
+    const renderGrid = () => {
+        if (error) return renderError();
+        return (
+            <section className="anime-chart">
+                <div className="anime-container">
+                    <div className="TV">
+                        <h3 className="type-heading white">TV</h3>
+                        {loading ? (
+                            renderLoading()
+                        ) : (
+                            <Grid
+                                container
+                                justify="center"
+                                direction="row"
+                                alignItems="center"
+                                spacing={2}
+                            >
+                                {renderTVanime()}
+                            </Grid>
+                        )}
+                    </div>
+                    <div className="ONA">
+                        <h3 className="type-heading black top-padding">ONA</h3>
+                        {loading ? (
+                            renderLoading()
+                        ) : (
+                            <Grid
+                                container
+                                justify="center"
+                                direction="row"
+                                alignItems="center"
+                                spacing={2}
+                            >
+                                {renderONAanime()}
+                            </Grid>
+                        )}
+                    </div>
+                    <div className="Movies">
+                        <h3 className="type-heading black top-padding">
+                            Movies
+                        </h3>
+                        {loading ? (
+                            renderLoading()
+                        ) : (
+                            <Grid
+                                container
+                                justify="center"
+                                direction="row"
+                                alignItems="center"
+                                spacing={2}
+                            >
+                                {renderMoviesanime()}
+                            </Grid>
+                        )}
+                    </div>
+                </div>
+            </section>
+        );
+    };
 
-	const renderGrid = () => {
-		if (error) return renderError();
-		return (
-			<section className="anime-chart">
-				<div className="anime-container">
-					<div className="TV">
-						<h3 className="type-heading white">TV</h3>
-						{loading ? (
-							renderLoading()
-						) : (
-							<Grid
-								container
-								justify="center"
-								direction="row"
-								alignItems="center"
-								spacing={2}
-							>
-								{renderTVanime()}
-							</Grid>
-						)}
-					</div>
-					<div className="ONA">
-						<h3 className="type-heading black top-padding">ONA</h3>
-						{loading ? (
-							renderLoading()
-						) : (
-							<Grid
-								container
-								justify="center"
-								direction="row"
-								alignItems="center"
-								spacing={2}
-							>
-								{renderONAanime()}
-							</Grid>
-						)}
-					</div>
-					<div className="Movies">
-						<h3 className="type-heading black top-padding">Movies</h3>
-						{loading ? (
-							renderLoading()
-						) : (
-							<Grid
-								container
-								justify="center"
-								direction="row"
-								alignItems="center"
-								spacing={2}
-							>
-								{renderMoviesanime()}
-							</Grid>
-						)}
-					</div>
-				</div>
-			</section>
-		);
-	};
+    const renderTVanime = () => {
+        const tv = anime.anime.filter(
+            (anime) => anime.attributes.showType === "TV"
+        );
+        return tv.map(({ id, attributes }) => (
+            <Grid item xs={12} sm={6} md={3} key={id}>
+                <AnimeCard
+                    name={attributes.titles.en_jp}
+                    image={attributes.posterImage.small}
+                    des={attributes.description}
+                    videoID={attributes.youtubeVideoId}
+                />
+            </Grid>
+        ));
+    };
 
-	const renderTVanime = () => {
-		const tv = anime.anime.filter(
-			(anime) => anime.attributes.showType === "TV"
-		);
-		return tv.map(({ id, attributes }) => (
-			<Grid item xs={12} sm={6} md={3} key={id}>
-				<AnimeCard
-					name={attributes.titles.en_jp}
-					image={attributes.posterImage.small}
-					des={attributes.description}
-					videoID={attributes.youtubeVideoId}
-				/>
-			</Grid>
-		));
-	};
+    const renderONAanime = () => {
+        const tv = anime.anime.filter(
+            (anime) => anime.attributes.showType === "ONA"
+        );
+        return tv.map(({ id, attributes }) => (
+            <Grid item xs={12} sm={6} md={3} key={id}>
+                <AnimeCard
+                    name={attributes.titles.en_jp}
+                    image={attributes.posterImage.small}
+                    des={attributes.description}
+                    videoID={attributes.youtubeVideoId}
+                />
+            </Grid>
+        ));
+    };
 
-	const renderONAanime = () => {
-		const tv = anime.anime.filter(
-			(anime) => anime.attributes.showType === "ONA"
-		);
-		return tv.map(({ id, attributes }) => (
-			<Grid item xs={12} sm={6} md={3} key={id}>
-				<AnimeCard
-					name={attributes.titles.en_jp}
-					image={attributes.posterImage.small}
-					des={attributes.description}
-					videoID={attributes.youtubeVideoId}
-				/>
-			</Grid>
-		));
-	};
+    const renderMoviesanime = () => {
+        const tv = anime.anime.filter(
+            (anime) => anime.attributes.showType === "movie"
+        );
+        return tv.map(({ id, attributes }) => (
+            <Grid item xs={12} sm={6} md={3} key={id}>
+                <AnimeCard
+                    name={attributes.titles.en_jp}
+                    image={attributes.posterImage.small}
+                    des={attributes.description}
+                    videoID={attributes.youtubeVideoId}
+                />
+            </Grid>
+        ));
+    };
 
-	const renderMoviesanime = () => {
-		const tv = anime.anime.filter(
-			(anime) => anime.attributes.showType === "movie"
-		);
-		return tv.map(({ id, attributes }) => (
-			<Grid item xs={12} sm={6} md={3} key={id}>
-				<AnimeCard
-					name={attributes.titles.en_jp}
-					image={attributes.posterImage.small}
-					des={attributes.description}
-					videoID={attributes.youtubeVideoId}
-				/>
-			</Grid>
-		));
-	};
-
-	return <div className="home-page">{renderGrid()}</div>;
+    return <div className="home-page">{renderGrid()}</div>;
 };
 
 const mapstateToProps = (state) => {
-	return {
-		anime: state.animeReducer.anime,
-		loading: state.animeReducer.loading,
-		error: state.animeReducer.error,
-	};
+    return {
+        anime: state.animeReducer.anime,
+        loading: state.animeReducer.loading,
+        error: state.animeReducer.error,
+    };
 };
 
 export default connect(mapstateToProps)(Home);
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -3,50 +3,62 @@ import React from "react";
 import "./Nav.scss";
 
 const Nav = ({ season, active }) => {
-	return (
-		<div className="nav-container">
-			<div className="logo-container">AniSeason</div>
-			<div className="seasons-container">
-				<a
-					className="winter-2020"
-					style={active === "winter" ? { color: "white" } : {}}
-					onClick={() => season("winter")}
-				>
-					Winter
-					<div>2020</div>
-				</a>
-				<a
-					className="spring-2020"
-					style={active === "spring" ? { color: "white" } : {}}
-					onClick={() => season("spring")}
-				>
-					Spring
-					<div>2020</div>
-				</a>
-				<a
-					className="summer-2020"
-					style={active === "summer" ? { color: "white" } : {}}
-					onClick={() => season("summer")}
-				>
-					Summer
-					<div>2020</div>
-				</a>
-				<a
-					className="fall-2020"
-					style={active === "fall" ? { color: "white" } : {}}
-					onClick={() => season("fall")}
-				>
-					Fall
-					<div>2020</div>
-				</a>
-			</div>
-			<div className="login-container">
-				<a className="login" href="/login">
-					Login
-				</a>
-			</div>
-		</div>
-	);
+    const mobileNav = () => {
+        return (
+            <div className="mobile-nav-container">
+                <div className="logo-container"></div>
+            </div>
+        );
+    };
+
+    const renderNav = () => {
+        return (
+            <div className="nav-container">
+                <div className="logo-container">AniSeason</div>
+                <div className="seasons-container">
+                    <a
+                        className="winter-2020"
+                        style={active === "winter" ? { color: "white" } : {}}
+                        onClick={() => season("winter")}
+                    >
+                        Winter
+                        <div>2020</div>
+                    </a>
+                    <a
+                        className="spring-2020"
+                        style={active === "spring" ? { color: "white" } : {}}
+                        onClick={() => season("spring")}
+                    >
+                        Spring
+                        <div>2020</div>
+                    </a>
+                    <a
+                        className="summer-2020"
+                        style={active === "summer" ? { color: "white" } : {}}
+                        onClick={() => season("summer")}
+                    >
+                        Summer
+                        <div>2020</div>
+                    </a>
+                    <a
+                        className="fall-2020"
+                        style={active === "fall" ? { color: "white" } : {}}
+                        onClick={() => season("fall")}
+                    >
+                        Fall
+                        <div>2020</div>
+                    </a>
+                </div>
+                <div className="login-container">
+                    <a className="login" href="/login">
+                        Login
+                    </a>
+                </div>
+            </div>
+        );
+    };
+
+    return <>{renderNav()}</>;
 };
 
 export default Nav;
